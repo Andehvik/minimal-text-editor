a354299cfd8c5f86790cb81581f340f2de31a4e0
Andehvik <andershvik@gmail.com>
2025-09-17 21:04:56 +0200

    format file

--- main.c
+++ main.c
@@ -1,82 +1,73 @@
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 
-
 struct termios info;
-void read_from_file(){
+void read_from_file() {
 
-  
-tcgetattr(0, &info);
-info.c_lflag &= ~ICANON;
+  tcgetattr(0, &info);
+  info.c_lflag &= ~ICANON;
   info.c_cc[VMIN] = 1;
   info.c_cc[VTIME] = 0;
-  tcsetattr(0,TCSANOW,&info);
+  tcsetattr(0, TCSANOW, &info);
 
-  FILE * inn =  fopen("/home/anders/Dev/min_editor/new.txt", "r+");
-  char buffer[50]; 
+  FILE *inn = fopen("/home/anders/Dev/min_editor/new.txt", "r+");
+  char buffer[50];
 
-
-  if(errno != 0){
+  if (errno != 0) {
 
     perror("fopen");
     return exit(EXIT_FAILURE);
-  }else{
+  } else {
 
     printf("\x1b[2J");
     printf("\x1b[H");
     printf("\x1b[2K");
     int c;
-    while((c = fgetc(inn))!= EOF)
+    while ((c = fgetc(inn)) != EOF)
       putchar(c);
 
-    if(ferror(inn))
+    if (ferror(inn))
       puts("I/O error when reading");
-    else if(feof(inn))
-    {
+    else if (feof(inn)) {
       puts("End of file is reached successfully");
     }
 
-    while(1){
+    while (1) {
 
-      if(getchar() == 107){
+      if (getchar() == 107) {
         printf("\x1b[1A");
       }
-      if(getchar() == 106){
+      if (getchar() == 106) {
         printf("\x1b[1B");
       }
     }
 
-  fgets(buffer, sizeof(buffer), stdin);
-    fwrite(buffer,sizeof(char),strlen(buffer),inn);
-
+    fgets(buffer, sizeof(buffer), stdin);
+    fwrite(buffer, sizeof(char), strlen(buffer), inn);
 
     printf("%d", strlen(buffer));
-  fclose(inn);
-    tcgetattr(0,&info);
+    fclose(inn);
+    tcgetattr(0, &info);
     info.c_lflag |= ICANON;
     tcsetattr(0, TCSANOW, &info);
   }
-
-  
 };
 
-void input(){
+void input() {
 
   printf("Hello World! type something\n");
-	char input [20];
+  char input[20];
 
-	fgets(input, sizeof(input), stdin);
-	if(errno != 0){
-		perror("fgets");
-	}
-	printf("%s\n",input);
-
+  fgets(input, sizeof(input), stdin);
+  if (errno != 0) {
+    perror("fgets");
+  }
+  printf("%s\n", input);
 };
-int main(void) { 
-	errno = 0;
+int main(void) {
+  errno = 0;
   read_from_file();
-
 }
